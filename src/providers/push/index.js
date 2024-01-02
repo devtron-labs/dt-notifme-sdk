349ee5615c7a6ef63dea81737b482236382e10a6
ritvikmahajan17 <ritvikmahajan2001@gmail.com>
2024-01-02 17:59:49 +0530

    fix: uncomment used code

--- src/providers/push/index.js
+++ src/providers/push/index.js
@@ -1,45 +1,45 @@
-// /* @flow */
-// import PushAdmProvider from './adm'
-// import PushApnProvider from './apn'
-// import PushFcmProvider from './fcm'
-// import PushLoggerProvider from '../logger'
-// import PushNotificationCatcherProvider from './notificationCatcher'
-// import PushWnsProvider from './wns'
-// // Types
-// import type { PushRequestType } from '../../models/notification-request'
+/* @flow */
+import PushAdmProvider from './adm'
+import PushApnProvider from './apn'
+import PushFcmProvider from './fcm'
+import PushLoggerProvider from '../logger'
+import PushNotificationCatcherProvider from './notificationCatcher'
+import PushWnsProvider from './wns'
+// Types
+import type { PushRequestType } from '../../models/notification-request'
 
-// export interface PushProviderType {
-//   id: string,
-//   send(request: PushRequestType): Promise<string>
-// }
+export interface PushProviderType {
+  id: string,
+  send(request: PushRequestType): Promise<string>
+}
 
-// export default function factory ({ type, ...config }: Object): PushProviderType {
-//   switch (type) {
-//     // Development
-//     case 'logger':
-//       return new PushLoggerProvider(config, 'push')
+export default function factory ({ type, ...config }: Object): PushProviderType {
+  switch (type) {
+    // Development
+    case 'logger':
+      return new PushLoggerProvider(config, 'push')
 
-//     case 'notificationcatcher':
-//       return new PushNotificationCatcherProvider('push')
+    case 'notificationcatcher':
+      return new PushNotificationCatcherProvider('push')
 
-//     // Custom
-//     case 'custom':
-//       return config
+    // Custom
+    case 'custom':
+      return config
 
-//     // Providers
-//     case 'adm':
-//       return new PushAdmProvider(config)
+    // Providers
+    case 'adm':
+      return new PushAdmProvider(config)
 
-//     case 'apn':
-//       return new PushApnProvider(config)
+    case 'apn':
+      return new PushApnProvider(config)
 
-//     case 'fcm':
-//       return new PushFcmProvider(config)
+    case 'fcm':
+      return new PushFcmProvider(config)
 
-//     case 'wns':
-//       return new PushWnsProvider(config)
+    case 'wns':
+      return new PushWnsProvider(config)
 
-//     default:
-//       throw new Error(`Unknown push provider "${type}".`)
-//   }
-// }
+    default:
+      throw new Error(`Unknown push provider "${type}".`)
+  }
+}
--- src/providers/push/wns.js
+++ src/providers/push/wns.js
@@ -1,30 +1,30 @@
-/* @flow */
-import PushNotifications from 'node-pushnotifications'
-// Types
-import type { PushRequestType } from '../../models/notification-request'
+// /* @flow */
+// import PushNotifications from 'node-pushnotifications'
+// // Types
+// import type { PushRequestType } from '../../models/notification-request'
 
-export default class PushWnsProvider {
-  id: string = 'push-wns-provider'
-  transporter: Object
+// export default class PushWnsProvider {
+//   id: string = 'push-wns-provider'
+//   transporter: Object
 
-  constructor (config: Object) {
-    this.transporter = new PushNotifications({
-      wns: {
-        ...config,
-        client_id: config.clientId,
-        client_secret: config.clientSecret
-      }
-    })
-  }
+//   constructor (config: Object) {
+//     this.transporter = new PushNotifications({
+//       wns: {
+//         ...config,
+//         client_id: config.clientId,
+//         client_secret: config.clientSecret
+//       }
+//     })
+//   }
 
-  async send (request: PushRequestType): Promise<string> {
-    const { registrationToken, ...rest } =
-      request.customize ? (await request.customize(this.id, request)) : request
-    const result = await this.transporter.send([registrationToken], rest)
-    if (result[0].failure > 0) {
-      throw new Error(result[0].message[0].error)
-    } else {
-      return result[0].message[0].messageId
-    }
-  }
-}
+//   async send (request: PushRequestType): Promise<string> {
+//     const { registrationToken, ...rest } =
+//       request.customize ? (await request.customize(this.id, request)) : request
+//     const result = await this.transporter.send([registrationToken], rest)
+//     if (result[0].failure > 0) {
+//       throw new Error(result[0].message[0].error)
+//     } else {
+//       return result[0].message[0].messageId
+//     }
+//   }
+// }
